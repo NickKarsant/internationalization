a0014a71bf667c3afa8e4d2b2e8de85e8848a545
Nicholas Karsant <nkarsant@gmail.com>
2021-02-13 22:08:12 -0800

    add second context value requires use of Consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,60 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from '../contexts/LanguageContext'
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
-  state = { language: 'english' };
+  state = { language: "english" };
 
-
-  onLanguageChange = (language) => {
-    this.setState({language: language});
-  }
+  onLanguageChange = language => {
+    this.setState({ language: language });
+  };
 
   render() {
     return (
       <div className="ui container">
-        <div>Select a Langauge: 
-        <i className="flag us" onClick={() => this.onLanguageChange('english')}  style={{marginLeft: '5px'}} ></i>
-        <i className="flag fr" onClick={() => this.onLanguageChange('french')}></i>
-        <i className="flag gr" onClick={() => this.onLanguageChange('greek')}></i>
-        <i className="flag am" onClick={() => this.onLanguageChange('armenian')}></i>
-        <i className="flag cn" onClick={() => this.onLanguageChange('mandarin')}></i>
-        <i className="flag mx" onClick={() => this.onLanguageChange('spanish')}></i>
-        <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
+        <div>
+          Select a Langauge:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+            style={{ marginLeft: "5px" }}
+          ></i>
+          <i
+            className="flag fr"
+            onClick={() => this.onLanguageChange("french")}
+          ></i>
+          <i
+            className="flag gr"
+            onClick={() => this.onLanguageChange("greek")}
+          ></i>
+          <i
+            className="flag am"
+            onClick={() => this.onLanguageChange("armenian")}
+          ></i>
+          <i
+            className="flag cn"
+            onClick={() => this.onLanguageChange("mandarin")}
+          ></i>
+          <i
+            className="flag mx"
+            onClick={() => this.onLanguageChange("spanish")}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          ></i>
         </div>
 
         {this.state.language}
-        <LanguageContext.Provider value={this.state.language}>
-        <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value={"green"}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
 }
 
 export default App;
- 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import LanguageContext from '../contexts/LanguageContext';
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
-
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Tíjiāo';
-    return <button className='ui button mini primary'>{text}</button>;
+    return (
+      <ColorContext.Consumer>
+
+      {(color) => (
+        <button className={`ui button mini ${color}`}>
+          <LanguageContext.Consumer>
+            {value => (value === "english" ? "Submit" : "Tíjiāo")}
+          </LanguageContext.Consumer>
+        </button>
+      )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
