6e72c64dc55ceaef0540cceda0d2e01a1b8ee8fc
Nicholas Karsant <nkarsant@gmail.com>
2021-02-13 18:50:44 -0800

    all components started

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+
+  onLanguageChange = (language) => {
+    this.setState({language: language});
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>Select a Langauge: 
+        <i style={{marginLeft: '5px'}} className="flag us" onClick={() => this.onLanguageChange('english')}></i>
+        <i className="flag fr" onClick={() => this.onLanguageChange('french')}></i>
+        <i className="flag gr" onClick={() => this.onLanguageChange('greek')}></i>
+        <i className="flag am" onClick={() => this.onLanguageChange('armenian')}></i>
+        <i className="flag cn" onClick={() => this.onLanguageChange('mandarin')}></i>
+        <i className="flag mx" onClick={() => this.onLanguageChange('spanish')}></i>
+        <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
---
+++ src/components/Field.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+
+class Field extends React.Component {
+
+  render(){
+    return <div>Field</div>
+  }
+}
+
+export default Field;
